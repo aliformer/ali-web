b4b9a78723ad46164733423d9d7d54b99db4c754
aliformer <10822957-aliformer@users.noreply.gitlab.com>
2022-08-21 19:53:38 +0700

    create boilerplate web

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  let open = false
+  const toggle = () => {
+    open = !open
+  }
+</script>
+
+<header class="header "> 
+  <button class="menu-button" on:click="{toggle}">
+    <svg viewBox="0 0 60 60" width="20" height="20">
+      <rect width="100" height="10"></rect>
+      <rect y="20" width="100" height="10"></rect>
+      <rect y="40" width="100" height="10"></rect>
+    </svg>
+  </button>
+</header>
+<div class="wrapper {open === false ? 'nogrid' : ""}"> 
+  <nav class="nav {open === true ? 'display': '' }">
+    <ul class="nav-list">
+      <li><a class="nav-link" href="#">Home</a></li>
+      <li><a class="nav-link" href="#">About</a></li>
+      <li><a class="nav-link" href="#">Contact</a></li>
+    </ul> 
+  </nav>
+  <main class="main">
+  </main>
+</div>
+
